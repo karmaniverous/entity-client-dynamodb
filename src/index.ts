1aa4ba8926ff6f0c99734d55cf2d4c9cbd9a0927
Jason Williscroft <me@karmanivero.us>
2024-10-07 17:49:27 +0800

    exposed Item type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { EntityManagerClientDynamoDb } from './EntityManagerClientDynamoDb';
 export type { EntityManagerClientDynamoDbOptions } from './EntityManagerClientDynamoDbOptions';
 export type { GetItemOptions } from './GetItemOptions';
+export type { Item } from './Item';
 export type { EntityManagerClientBatchOptions } from '@karmaniverous/entity-manager';
